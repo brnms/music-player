48257965c60ddd0f55ba01972a05da40be6f26ea
Bruno Santos <bruno.matasantos@gmail.com>
2020-09-16 09:49:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const musicContainer = document.getElementById('music-container');
+const musicLyrics = document.getElementById('music-lyrics');
+const lyrics = document.getElementById('lyrics');
 const playBtn = document.getElementById('play');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
@@ -10,7 +12,7 @@ const title = document.getElementById('title');
 const cover = document.getElementById('cover');
 
 // Song titles
-let songs = ['hey', 'summer', 'ukulele'];
+let songs = ['Eluveitie - Breathe', 'summer', 'Nightwish - Pan'];
 
 // Keep track of song
 let songIndex = 2;
@@ -31,7 +33,7 @@ function playSong() {
     musicContainer.classList.add('play');
     playBtn.querySelector('i.fas').classList.remove('fa-play');
     playBtn.querySelector('i.fas').classList.add('fa-pause');
-
+    showLyrics()
     audio.play()
 }
 // Pause song
@@ -40,7 +42,7 @@ function pauseSong() {
     musicContainer.classList.remove('play');
     playBtn.querySelector('i.fas').classList.add('fa-play');
     playBtn.querySelector('i.fas').classList.remove('fa-pause');
-
+    musicLyrics.style.opacity = '0'
     audio.pause()
 }
 
@@ -81,8 +83,26 @@ function setProgress(e) {
     const duration = audio.duration;
 
     audio.currentTime = (clickX / width) * duration;
+}
 
-    console.log(clickX)
+// Show lyrics
+function showLyrics() {
+musicLyrics.style.opacity = '1'
+   lyrics.innerHTML = `
+        Once there was a silent canvas
+        Sleeping stories unimagined
+        Birth of what if's, hope and wonder
+        Winds will be named, words will shelter
+        Then, something wicked their way came
+        Showed a way to the great escape
+        Evoke the worlds, sparked the brain
+        An ape in awe before a door
+        To labyrinth
+        To keystone earth
+        To fallen stars
+        To there and back
+        We're the writers of another way to be  
+   `
 }
 
 // Event listeners
